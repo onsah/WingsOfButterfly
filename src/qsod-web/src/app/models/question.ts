cea1776ba59658b24e3a863be74744367ebb1068
onur <sahinonur2000@hotmail.com>
2020-05-21 13:53:38 +0300

    added quiz edit/create page

--- src/qsod-web/src/app/models/question.ts
+++ src/qsod-web/src/app/models/question.ts
@@ -1,24 +1,121 @@
 import { QuestionID, Difficulty, Tag } from './types';
 
 export class Question {
-    id: QuestionID;
-    description: string;
-    difficulty: Difficulty;
-    correctOption: Option;
-    options: string[];  // Always 4
-    tags: Tag[];
-    image: Uint8Array;
+
+    private constructor(
+        public id: QuestionID,
+        public description: string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et",
+        public difficulty: Difficulty = Difficulty.Easy,
+        public correctOption: Option = Option.A,
+        public options: string[] = ["Option A", "Option B", "Option C", "Option D"],
+        public tags: Tag[] = [ 'A', 'B' ],
+        public image: Uint8Array = null,
+    ) {
+    }
 
     static getDefault(id: number = 0): Question {
-        return {
-            id,
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-            difficulty: Difficulty.Easy,
-            correctOption: Option.A,
-            options: ["Option A", "Option B", "Option C", "Option D"] ,
-            tags: [ 'A', 'B' ],
-            image: new Uint8Array(),
-        }
+        return new Question(id);
+    }
+
+    get correctOptionAsArray(): boolean[] {
+        switch (this.correctOption) {
+            case Option.A:
+                return [true, false, false, false];
+            case Option.B:
+                return [false, true, false, false];
+            case Option.C:
+                return [false, false, true, false];
+            case Option.D:
+                return [false, false, false, true];
+            case Option.AB:
+                return [true, true, false, false];
+            case Option.AC:
+                return [true, false, true, false];
+            case Option.AD:
+                return [true, false, false, true];
+            case Option.BC:
+                return [false, true, true, false];
+            case Option.BD:
+                return [false, true, false, true];
+            case Option.CD:
+                return [false, false, true, true];
+            case Option.ABC:
+                return [true, true, true, false];
+            case Option.ABD:
+                return [true, true, false, true];
+            case Option.BCD:
+                return [false, true, true, true];
+            case Option.ACD:
+                return [true, false, true, true];
+            case Option.ABCD:
+                return [true, true, true, true];
+            case Option.Empty:
+                return [false, false, false, false];                        
+        }   
+    }
+
+    set correctOptionAsArray(options: boolean[]) {
+        if (options[0]) {
+            if (options[1]) {
+                if (options[2]) {
+                    if (options[3]) {
+                        this.correctOption = Option.ABCD;
+                    } else {
+                        this.correctOption = Option.ABC;
+                    }
+                } else {
+                    if (options[3]) {
+                        this.correctOption = Option.ABD;
+                    } else {
+                        this.correctOption = Option.AB;
+                    }
+                }
+            } else {
+                if (options[2]) {
+                    if (options[3]) {
+                        this.correctOption = Option.ACD;
+                    } else {
+                        this.correctOption = Option.AC;
+                    }
+                } else {
+                    if (options[3]) {
+                        this.correctOption = Option.AD;
+                    } else {
+                        this.correctOption = Option.A;
+                    }
+                }
+            }
+            } else {
+            if (options[1]) {
+                if (options[2]) {
+                    if (options[3]) {
+                        this.correctOption = Option.BCD;
+                    } else {
+                        this.correctOption = Option.BC;
+                    }
+                } else {
+                    if (options[3]) {
+                        this.correctOption = Option.BD;
+                    } else {
+                        this.correctOption = Option.B;
+                    }
+                }
+            } else {
+                if (options[2]) {
+                    if (options[3]) {
+                        this.correctOption = Option.CD;
+                    } else {
+                        this.correctOption = Option.C;
+                    }
+                } else {
+                    if (options[3]) {
+                        this.correctOption = Option.D;
+                    } else {
+                        this.correctOption = Option.Empty;
+                    }
+                }
+            }
+        } 
     }
 }
 
--- src/qsod-web/src/app/quiz/quiz-create/quiz-create.component.ts
+++ src/qsod-web/src/app/quiz/quiz-create/quiz-create.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Quiz } from 'src/app/models/quiz';
+import { Question } from 'src/app/models/question';
+import { Tag } from 'src/app/models/types';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { QuizService } from 'src/app/services/quiz.service';
 
 @Component({
   selector: 'app-quiz-create',
@@ -7,19 +12,69 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./quiz-create.component.css']
 })
 export class QuizCreateComponent implements OnInit {
-
+  
   private isEditMode: boolean;
 
+  quiz: Quiz;
+  questions: Question[] = [];
+
+  private unselectedTagsStore: Tag[] = [];
+  private selectedTagsStore: Tag[] = [];
+  private _unselectedTags: BehaviorSubject<Tag[]> = new BehaviorSubject(this.unselectedTagsStore);
+  private _selectedTags: BehaviorSubject<Tag[]> = new BehaviorSubject(this.selectedTagsStore);
+
   constructor(
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private quizService: QuizService,
   ) { }
 
   ngOnInit(): void {
+
+    this.unselectedTagsStore = this.quizService.tags;
+    this._unselectedTags.next(this.unselectedTagsStore);
 
     this.route.paramMap.subscribe(params => {
       console.log(`isEditMode: ${params.get('isEditMode')}`);
 
       this.isEditMode = JSON.parse(params.get('isEditMode'));
-    })
+    });
+
+    this.quiz = Quiz.getDefault();
+    this.quiz.questionIDs.forEach(_ => this.questions.push(Question.getDefault()));
+  }
+
+  get unselectedTags(): Observable<Tag[]> {
+    return this._unselectedTags.asObservable();
+  }
+
+  get selectedTags(): Observable<Tag[]> {
+    return this._selectedTags.asObservable();
+  }
+
+  addTag(tag: Tag) {
+    if (tag == 'None') {
+      return;
+    }
+    let index = this.unselectedTagsStore.indexOf(tag);
+    if (index > -1) {
+      this.unselectedTagsStore.splice(index, 1);
+      this._unselectedTags.next(this.unselectedTagsStore);
+      
+      this.selectedTagsStore.push(tag);
+      this._selectedTags.next(this.selectedTagsStore);
+    }
+  }
+
+  removeTag(tag: Tag) {
+    if (tag == 'None') {
+      return;
+    }
+    let index = this.selectedTagsStore.indexOf(tag);
+
+    this.selectedTagsStore.splice(index, 1);
+    this._selectedTags.next(this.selectedTagsStore);
+
+    this.unselectedTagsStore.push(tag);
+    this._unselectedTags.next(this.unselectedTagsStore);
   }
 }
